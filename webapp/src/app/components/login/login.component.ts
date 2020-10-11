756391f808ed8d0e347e3b731a45fb178c53927f
Martin NAGY <martin.laszlo.nagy@gmail.com>
2020-10-11 20:43:18 +0200

    Add facebook login

--- webapp/src/app/components/login/login.component.ts
+++ webapp/src/app/components/login/login.component.ts
@@ -1,20 +1,61 @@
 import { Helpers } from './../../helpers/helpers';
 import { Component, OnInit } from '@angular/core';
 
+declare var FB: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private helpers: Helpers) {}
 
-  constructor(private helpers: Helpers) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: '778458156056746',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.1',
+      });
+      FB.AppEvents.logPageView();
+    };
+    document.body.classList.add('bg-img');
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
   }
 
-  login(){
+  loginDev() {
     this.helpers.authenticate();
+    document.body.classList.remove('bg-img');
   }
 
+  submitLogin() {
+    console.log('submit login to facebook');
+    console.log('logged in' + this.loggedIn);
+    // FB.login();
+    FB.login((response) => {
+      console.log('submitLogin', response);
+      if (response.authResponse) {
+        this.helpers.authenticate();
+
+        document.body.classList.remove('bg-img');
+        //login success
+        //login success code here
+        //redirect to home page
+      } else {
+        console.log('User login failed');
+      }
+    });
+  }
 }
