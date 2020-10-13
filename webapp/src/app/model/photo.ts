fe7dd8cc6665e9e33f97f5473476d4f657808880
Tamás Géczi <tamas.gczi@gmail.com>
2020-10-13 20:11:41 +0200

    Added test array

--- webapp/src/app/model/photo.ts
+++ webapp/src/app/model/photo.ts
@@ -5,3 +5,9 @@ export class PhotoCard{
 
 }
 
+export const PHOTOS: PhotoCard[] = [
+    {image: 'src/assets/img/ph1.jpg', username: 'uploader'},
+    {image: 'src/assets/img/ph2.jpg', username: 'uploader'},
+    {image: 'src/assets/img/ph3.jpg', username: 'uploader'},
+    {image: 'src/assets/img/ph4.jpg', username: 'uploader'},
+];
