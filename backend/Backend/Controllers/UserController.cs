9167ee007385a1332e029bdda27965bbc31fea3f
CD1H49 <almasi.patrik@gmail.com>
2020-10-18 00:09:36 +0200

    Created backend project.

---
+++ backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public ActionResult UserTest()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public ActionResult Dashboard(HttpRequestHeaders header)
+        {
+            var result = new System.Collections.ObjectModel.Collection<string>();
+            IEnumerable<string> values;
+            if (header.TryGetValues("CONNECTION-ID", out values))
+            {
+                return Ok();
+            }
+            return BadRequest();
+
+            HttpHeaders headers = response.Headers;
+            IEnumerable<string> values;
+            if (headers.TryGetValues("X-BB-SESSION", out values))
+            {
+                string session = values.First();
+            }
+        }
+    }
+}
---
+++ backend/Backend/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Model
+{
+    public class User
+    {
+        public string UID { get; set; }
+        public string ConnectionID { get; set; }
+        public string Email { get; set; }
+        public string ProfilePic { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<User> Friends { get; set; }
+        public List<string> Pictures { get; set; }
+        public string Bio { get; set; }
+    }
+}
