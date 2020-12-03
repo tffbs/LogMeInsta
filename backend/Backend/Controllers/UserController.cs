bd7f7d369497bf1199f979d7c041085fc79c50c9
CD1H49 <almasi.patrik@gmail.com>
2020-12-03 19:40:05 +0100

    //Authorized.

--- backend/Backend/Controllers/UserController.cs
+++ backend/Backend/Controllers/UserController.cs
@@ -15,7 +15,7 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("[controller]")]
     [ApiController]
     public class UserController : ControllerBase
